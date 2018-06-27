ae97ba328469d84120fc7596747cfb54006abed5
Brian Perry <contact.brianperry@gmail.com>
2018-06-27 11:25:57 -0400

    Added initial theme and css processing

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sassGlob = require('gulp-sass-glob');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('default', ['sass:watch']);
+
+gulp.task('sass', function () {
+  return gulp
+    .src('./sass/back_to_basics.scss')
+    .pipe(sassGlob())
+    .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Back to Basics Example Theme
+
+Example theme demonstrating some of the tooling and concepts in the 'Back to Basics 
+Drupal 8 Theming' talk.
+
+## Setup
+Requires NodeJS: https://nodejs.org/en/
+
+## Tooling
+* NPM - node dependencies, and also NPM Scripts for all front end tasks - see package.json
+* Gulp for CSS pre and post processing - see gulpfile.js - uses gulp-sass, gulp-sass, gulp-autoprefixer
